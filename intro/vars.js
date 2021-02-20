753674ffc80082a8f0223dd2e1788ef70c358c28
MattFerreira18 <matheusferreira.dev@gmail.com>
2021-02-20 19:50:24 -0300

    add types of vars

---
+++ intro/vars.js
@@ -0,0 +1,7 @@
+const Number = 5;
+const String = 'string';
+const bool = true; // or false;
+const u = undefined;
+const notANumber = NaN;
+const i = 1 / 0; // infinity
+const bInt = 10 ** 256 - 1; // bigInt
