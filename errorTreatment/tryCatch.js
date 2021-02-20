82eaa14567e654e108fffb9a8ebf6315cc37919a
MattFerreira18 <matheusferreira.dev@gmail.com>
2021-02-20 20:20:01 -0300

    add try-catch

---
+++ errorTreatment/tryCatch.js
@@ -0,0 +1,9 @@
+try {
+  // your code
+  console.log('try!');
+} catch (error) {
+  console.log(error);
+} finally {
+  // this code has every executed
+  // ex: close the archive that is open in TRY block
+}
