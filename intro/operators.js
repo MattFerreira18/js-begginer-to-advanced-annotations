8c33ffcf23cd9e1daa1f7b3c538869f5085d269a
MattFerreira18 <matheusferreira.dev@gmail.com>
2021-02-20 19:50:05 -0300

    add js operators

---
+++ intro/operators.js
@@ -0,0 +1,40 @@
+// In precedence order
+
+// Arithmetic
+const potentiation = 8 ** 2;
+const restDivision = 10 % 3;
+const multiply = 5 * 4;
+const division = 45 / 9;
+const sum = 5 + 4;
+const subtraction = 795 - 62;
+
+// attribution
+let number = 5;
+number **= 3;
+number %= 6;
+number *= 4;
+number /= 2;
+number += 3;
+number -= 67;
+
+// Unary
+number++; // pre-increment
+number--; // pre-decrement
+++number; // post-increment
+--number; // post-decrement
+
+// Comparation
+const resp = 4 === 3; // return false
+const greater = 18 > 9;
+const less = 18 < 9;
+const greaterOrEqual = 6 >= 0;
+const lessOrEqual = 5 <= 18;
+const inequal = 7 !== 4;
+
+// Logical
+/*
+&& => E
+|| => OR
+^ => XOR
+! => NOT
+*/
