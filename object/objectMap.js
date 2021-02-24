57e12a3a6b90f9b3c518154c2e799e2b2961f265
MattFerreira18 <matheusferreira.dev@gmail.com>
2021-02-23 22:35:54 -0300

    add object map method

---
+++ object/objectMap.js
@@ -0,0 +1,19 @@
+const peoples = [
+  { id: 1, name: 'user1' },
+  { id: 3, name: 'user2' },
+  { id: 2, name: 'user3' },
+];
+
+// const newPeoples = {};
+
+// for (const { id, name } of peoples) {
+//   newPeoples[id] = { id, name };
+// }
+
+const newPeoples = new Map();
+
+for (const { id, name } of peoples) {
+  newPeoples.set(id, { id, name });
+}
+
+console.log(newPeoples);
